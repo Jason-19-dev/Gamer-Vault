25da1501ac89bf222745a014b0a74421adcf69f8
aaddrrii <adrianasanchezaro@gmail.com>
2025-03-29 23:34:39 -0500

    signin page actualizada

--- src/app/auth/signin/signin.page.ts
+++ src/app/auth/signin/signin.page.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { IonButton, IonCard, IonCheckbox, IonContent, IonInput, IonInputPasswordToggle, IonText, AlertController } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import * as icons from 'ionicons/icons';
 import { Router } from '@angular/router';
-
+import { IonDatetime } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.page.html',
   styleUrls: ['./signin.page.scss'],
+  encapsulation: ViewEncapsulation.None,
   // standalone: true,
   imports: [
     IonContent,
@@ -21,6 +22,7 @@ import { Router } from '@angular/router';
     IonCheckbox,
     IonText,
     ReactiveFormsModule,
+    IonDatetime
 
   ]
 })
@@ -31,8 +33,9 @@ export class SigninPage implements OnInit {
   constructor(private fb: FormBuilder, private router: Router, private alertController: AlertController) {
     this.form_signin = this.fb.group(
       {
-        nameUser: ['', [Validators.required, Validators.maxLength(6)]],
-        email: ['', [Validators.email]],
+        nameUser: ['', [Validators.required, Validators.maxLength(25)]],
+        email: ['', [Validators.required, Validators.email]],
+        phoneNumber: ['', [Validators.required, Validators.pattern('^\\d{8}$')]],
         password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(8)]],
         password_confirm: ['', [Validators.required]],
 
