6600b05b884ab6f1a5c58a84f5d19da201fb6de4
JeremyAizprua <jeremy.aizprua@utp.ac.pa>
2025-04-27 14:21:00 -0500

    Shows the correct order history in the wallet and changes the styles

--- src/app/wallet/wallet.page.ts
+++ src/app/wallet/wallet.page.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { TabsPagesPage } from '../tabs_bar/tabs-pages/tabs-pages.page';
 import { ToastController } from '@ionic/angular';
 import { BiometricService } from 'src/services/biometric/biometric.service';
-import { Order, GameItem} from 'src/types';
-import { IonButton, IonButtons, IonCard, IonCol, IonContent, IonGrid, IonHeader, IonItem, IonMenuButton, IonRow, IonTitle, IonToast, IonToolbar } from '@ionic/angular/standalone';
+import { Order, GameItem } from 'src/types';
 import { Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { OrdersService } from 'src/services/orders/orders.service';
-import { jwtDecode } from 'jwt-decode';
-import { IonicModule } from '@ionic/angular'; // Importa IonicModule
+import { IonicModule } from '@ionic/angular';
+import { UserService } from 'src/services/user/user.service';
+import { Subscription } from 'rxjs';
+import { TabsPagesPage } from '../tabs_bar/tabs-pages/tabs-pages.page';
 
 @Component({
   selector: 'app-wallet',
@@ -18,26 +18,20 @@ import { IonicModule } from '@ionic/angular'; // Importa IonicModule
   styleUrls: ['./wallet.page.scss'],
   standalone: true,
   imports: [
-    IonContent,
-    IonHeader,
-    IonToolbar,
     CommonModule,
     FormsModule,
-    TabsPagesPage,
-    IonButton,
-    IonHeader,
-    IonTitle,
-    IonItem,
+    IonicModule,
     DatePipe,
-    IonicModule 
+    TabsPagesPage
   ]
 })
-export class WalletPage implements OnInit {
-
+export class WalletPage implements OnInit, OnDestroy {
   puntos: number = 6;
   saldo: number = 10.99;
   userId: string = '';
-  orders: any[] = [];
+  orders: Order[] = [];
+  private ordersSubscription?: Subscription;
+  private logoutSubscription?: Subscription;
 
   transactions: any[] = [
     {
@@ -55,31 +49,37 @@ export class WalletPage implements OnInit {
     private platform: Platform,
     private biometricService: BiometricService,
     private route: Router,
-    private ordersService: OrdersService
+    private ordersService: OrdersService,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
-    this.loadUserIdFromToken();
-    this.loadOrders();
+    this.loadUserAndOrders();
   }
 
-  loadUserIdFromToken() {
-    const authToken = localStorage.getItem('authToken');
-    if (authToken) {
-      try {
-        const decodedToken: any = jwtDecode(authToken);
-        this.userId = decodedToken.user_id;
-      } catch (error) {
-        console.error('Error al decodificar el token:', error);
-      }
+  ngOnDestroy() {
+    if (this.ordersSubscription) {
+      this.ordersSubscription.unsubscribe();
+    }
+    if (this.logoutSubscription) {
+      this.logoutSubscription.unsubscribe();
+    }
+  }
+
+
+  async loadUserAndOrders() {
+    this.userId = await this.userService.getCurrentUserID() || '';
+    if (this.userId) {
+      this.loadOrders();
     } else {
-      console.error('Token no encontrado');
+      console.error('No se pudo obtener el ID del usuario.');
+      this.toast_alert('bottom', 'Error al obtener la información del usuario.');
     }
   }
 
   loadOrders() {
     if (this.userId) {
-      this.ordersService.load_user_orders(this.userId).subscribe({
+      this.ordersSubscription = this.ordersService.load_user_orders(this.userId).subscribe({
         next: (res) => {
           this.orders = res || [];
           console.log("Orders loaded:", this.orders);
@@ -90,6 +90,8 @@ export class WalletPage implements OnInit {
         }
       });
     } else {
+      this.orders = [];
+      console.error('Usuario no autenticado');
       this.toast_alert('bottom', 'Usuario no autenticado');
     }
   }
@@ -152,6 +154,11 @@ export class WalletPage implements OnInit {
   }
 
   getOrderIconText(): string {
-    return 'Order'; //  Return a generic icon character
+    return 'Order';
+  }
+
+  viewDetailOrder(order: Order) {
+    console.log(order);
   }
 }
+
