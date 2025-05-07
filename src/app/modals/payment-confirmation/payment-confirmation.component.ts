c3a8c2c0a78601f9ec2e74c4e1c65bc494a594aa
carlosjesus <carlosjesusgc11@gmail.com>
2025-05-07 05:59:18 -0500

    change modals payment

--- src/app/modals/payment-confirmation/payment-confirmation.component.ts
+++ src/app/modals/payment-confirmation/payment-confirmation.component.ts
@@ -3,25 +3,27 @@ import { ModalController } from '@ionic/angular/standalone';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { UserService } from "src/services/user/user.service"
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-payment-confirmation',
   templateUrl: './payment-confirmation.component.html',
   styleUrls: ['./payment-confirmation.component.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule,CommonModule],
 })
 export class PaymentConfirmationComponent {
 
   @Input() isSuccess = false;
   @Input() isError = false;
   @Input() errorMessage = '';
+  @Input() redeemCode: string = 'BW-9837-XPZ5';
   username = this.userService.getCurrentUser()?.username
 
   constructor(private modalCtrl: ModalController, private router: Router,private userService: UserService) {}
 
   async handleButtonClick() {
     if (this.isSuccess) {
+      console.log("1");
       // Si es éxito: primero cerrar modal y luego navegar
       await this.modalCtrl.dismiss();
       await new Promise(resolve => setTimeout(resolve, 300)); // Pequeña pausa para que se vea el cierre
@@ -33,6 +35,7 @@ export class PaymentConfirmationComponent {
   }
   async goToWallet() {
     if (this.isSuccess) {
+      console.log("2");
       await this.modalCtrl.dismiss();
       await new Promise(resolve => setTimeout(resolve, 300)); // Pequeña pausa para que se vea el cierre
       this.router.navigate(['/wallet']);
@@ -40,6 +43,7 @@ export class PaymentConfirmationComponent {
       await this.modalCtrl.dismiss();
     }
   }
-@Input() redeemCode: string = 'BW-9837-XPZ5';
+
+
 
 }
